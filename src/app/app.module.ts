d7fbd4a7c6e182d87730cefa8b65c20aead576e0
Tahsin Ahmed <tahsin.ahmed23@brainstation-23.com>
2023-11-09 18:53:49 +0600

    add tailwind, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
